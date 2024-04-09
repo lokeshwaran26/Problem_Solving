57a8c45214501e24b957f591689892082f8d1ecd
lokesh <lokesh95664@gmail.com>
2024-04-09 20:06:53 +0530

    ....

--- LeetCode/Test.py
+++ LeetCode/Test.py
@@ -1,33 +1,15 @@
-# 345. Reverse Vowels of a String
+def largestRectangleArea ( heights):
+    maxArea = 0
+    stack = [ ] # pair: (index, height)
+    for i, h in enumerate (heights):
+        start = i
+        while stack and stack [-1][1] > h:
+            index, height = stack.pop()
+            maxArea = max(maxArea, height * (i - index))
+            start = index
+        stack.append((start, h))
+    for i, h in stack:
+        maxArea = max(maxArea, h * (len(heights) - i))
+    return maxArea
 
-# Example 1:
-
-# Input: s = "hello"
-# Output: "holle"
-# Example 2:
-
-# Input: s = "leetcode"
-# Output: "leotcede"
-
-def convert_vowels(s):
-    vowles = ['a','e','i','o','u','A','E','I','O','U']
-    sc = 'abcbnim'
-    m = list(sc)
-    print(m)
-    sr = list(s)
-    print(sr)
-    l = 0
-    r = len(sr) -1
-    while l < r:
-        if sr[l] in vowles and sr[r] in vowles:
-            sr[l],sr[r] = s[r],s[l]
-            l+=1
-            r-=1
-        if s[l] not in vowles:
-            l+=1
-        if s[r] not in vowles:
-            r-=1
-    return ''.join(sr) 
-
-print(convert_vowels('hello'))
-    
+print(largestRectangleArea([2, 1,5,6,2,3]))
