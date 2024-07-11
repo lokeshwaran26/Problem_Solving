4bf535fb11acba59771af3212fcead70fd712722
lokesh <lokesh95664@gmail.com>
2024-07-11 14:13:04 +0530

    .....

---
+++ LeetCode/container_with_most_water.py
@@ -0,0 +1,31 @@
+# 11. Container With Most Water
+# Solved
+# Medium
+# Topics
+# Companies
+# Hint
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are(i, 0) and (i, height[i]).
+
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+# Return the maximum amount of water a container can store.
+
+# Notice that you may not slant the container.
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            max_area = max(max_area, (right - left) *
+                           min(height[left], height[right]))
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
---
+++ LeetCode/sub_sequence.py
@@ -0,0 +1,30 @@
+# 392. Is Subsequence
+# Solved
+# Easy
+# Topics
+# Companies
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+# Example 2:
+
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+
+
+def isSubsequence(s, t):
+
+    ptr_s, ptr_t = 0, 0
+    
+    while ptr_t < len(t):
+        if ptr_s < len(s) and s[ptr_s] == t[ptr_t]:
+          ptr_s += 1
+        ptr_t += 1
+    return ptr_s == len(s)
