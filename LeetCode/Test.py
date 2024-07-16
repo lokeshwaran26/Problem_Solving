4c7ae02180b60d843c7190b18bb99eedb078f28b
lokesh <lokesh95664@gmail.com>
2024-07-16 15:57:11 +0530

    Solved leetcode prmls

--- LeetCode/Test.py
+++ LeetCode/Test.py
@@ -1,15 +1,19 @@
-def largestRectangleArea ( heights):
-    maxArea = 0
-    stack = [ ] # pair: (index, height)
-    for i, h in enumerate (heights):
-        start = i
-        while stack and stack [-1][1] > h:
-            index, height = stack.pop()
-            maxArea = max(maxArea, height * (i - index))
-            start = index
-        stack.append((start, h))
-    for i, h in stack:
-        maxArea = max(maxArea, h * (len(heights) - i))
-    return maxArea
+# def largestRectangleArea ( heights):
+#     maxArea = 0
+#     stack = [ ] # pair: (index, height)
+#     for i, h in enumerate (heights):
+#         start = i
+#         while stack and stack [-1][1] > h:
+#             index, height = stack.pop()
+#             maxArea = max(maxArea, height * (i - index))
+#             start = index
+#         stack.append((start, h))
+#     for i, h in stack:
+#         maxArea = max(maxArea, h * (len(heights) - i))
+#     return maxArea
 
-print(largestRectangleArea([2, 1,5,6,2,3]))
+# print(largestRectangleArea([2, 1,5,6,2,3]))
+
+n = [5,8,3,7,9,1]
+m = len(n)
+print(n[1:] + n[:1])
