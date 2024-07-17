524f85782a983a67e83314434d537b033b2e07a0
lokesh <lokesh95664@gmail.com>
2024-07-17 15:21:23 +0530

    ...

---
+++ LeetCode/palindrome.py
@@ -0,0 +1,14 @@
+# Sum of N numbers
+
+# List ---> [1,2,3,4,5,6,7,8,9,18]
+li = [10, 20, 8 ]
+n = len(li)
+# print(li[3])
+# print(n)
+sum = 0
+for i in range(n):
+    sum = sum + li[i]
+
+print(sum)
+
+
